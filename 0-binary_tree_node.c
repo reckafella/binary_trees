376cb31ed37b246485f76cbfd5b01967478990f8
reckafella <ethanmuthoniw6@gmail.com>
2023-08-03 13:48:21 -0700

    update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,15 +16,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 
-	if (!parent)
-		parent = new_node;
-	else
-	{
-		new_node->parent = parent;
-		new_node->n = value;
-        	new_node->left = NULL;
-	        new_node->right = NULL;
-	}
+	new_node->parent = parent;
+	new_node->n = value;
+       	new_node->left = NULL;
+        new_node->right = NULL;
 
 	return (new_node);
 }
