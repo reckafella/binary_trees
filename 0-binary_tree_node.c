55413f647a50bc61d7d50c550b5d618261f65712
reckafella <ethanmuthoniw6@gmail.com>
2023-08-03 13:30:43 -0700

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,6 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	/*  */
 	binary_tree_t *new_node = NULL;
 
 	new_node = malloc(sizeof(binary_tree_t));
@@ -17,14 +16,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
 	if (!parent)
 		parent = new_node;
 	else
+	{
 		new_node->parent = parent;
+		new_node->n = value;
+        	new_node->left = NULL;
+	        new_node->right = NULL;
+	}
 
 	return (new_node);
 }
